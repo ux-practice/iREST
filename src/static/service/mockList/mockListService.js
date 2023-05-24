9c25e01899776b32e382d51a2b2632f70e04f7d5
Maneesh Thareja <maneesh.thareja@impetus.com>
2023-05-24 14:03:29 +0000

    iREST: fixed port issue

--- src/static/service/mockList/mockListService.js
+++ src/static/service/mockList/mockListService.js
@@ -1,6 +1,6 @@
 import client from '../axiosInstance/axiosInstance'
 
-const mockListService = () => {
+const mockListService = ({data}) => {
   const {API_BASE_URL} = process.env
   const API_ENDPOINT = `${API_BASE_URL}/mock/list`
 
